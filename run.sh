fad6ab089051467c97c3acb95aa56d5389b6eba4
seohl16 <selim@student.42seoul.kr>
2022-06-07 13:20:57 +0000

    faceboxes

--- run.sh
+++ run.sh
@@ -1,23 +1,28 @@
 cp util.py mtcnn_test/util.py
 cp util.py retina_test/util.py
 cp util.py yolo_test/util.py
-IDIR="../data/dest_images/kakao/kakao2.jpeg" #FIXME
-NUM=kakao2 #FIXME
+IDIR="../data/dest_images/stock.jpeg" #FIXME
+NUM=stock #FIXME
 
-cd mtcnn_test
-python main.py --process_target 'Image' --image_dir $IDIR --debug_mode True
+# cd mtcnn_test
+# python main.py --process_target 'Image' --image_dir $IDIR --debug_mode True
+# mv ./saved/output.jpg ../results/$NUM-mtcnn.jpg
 
-cd ../retina_test 
-python main.py  --process_target 'Image' --image_dir $IDIR --debug_mode True 
 
-cd ../yolo_test 
-python main.py  --process_target 'Image' --image_dir $IDIR --debug_mode True
+# cd ../retina_test 
+# python main.py  --process_target 'Image' --image_dir $IDIR --debug_mode True 
+# mv ./saved/output.jpg ../results/$NUM-retina.jpg
 
-cd ../opencv_test 
-python opencvtest.py --image_dir $IDIR 
+# cd ../yolo_test 
+# python main.py  --process_target 'Image' --image_dir $IDIR --debug_mode True
+# mv ./saved/output.jpg ../results/$NUM-yolo.jpg
+
+# cd ../opencv_test 
+# python opencvtest.py --image_dir $IDIR 
+# mv ./saved/output.jpg ../results/$NUM-opencv.jpg
+
+cd faceboxes_test 
+python infer.py --image_dir $IDIR 
+mv ./saved/output.jpg ../results/$NUM-faceboxes.jpg
 
 cd ../
-mv mtcnn_test/saved/output.jpg ./results/$NUM-mtcnn.jpg
-mv retina_test/saved/output.jpg ./results/$NUM-retina.jpg
-mv yolo_test/saved/output.jpg ./results/$NUM-yolo.jpg
-mv opencv_test/output.jpg ./results/$NUM-opencv.jpg
